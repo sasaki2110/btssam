4a1753471809f09c7593871bf6631ebd0ee28062
sasaki2110 <sasaki2110@gmail.com>
2023-12-23 21:05:16 +0900

    add auth

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import SSRProvider from 'react-bootstrap/SSRProvider';
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
